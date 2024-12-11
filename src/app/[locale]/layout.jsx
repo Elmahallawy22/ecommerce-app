d88bde32b4cf3bd78a0bcee3a1fce04729909f20
Elmahallawy22 <elmahallawyyoussef@gmail.com>
2024-12-11 22:12:29 +0200

    try to solve layout problem

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -1,26 +1,24 @@
 import "./globals.css";
-
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
-import { notFound } from 'next/navigation';
-import { routing } from '../../i18n/routing';
+// import { notFound } from 'next/navigation';
+// import { routing } from '../../i18n/routing';
 import Header from "../../components/header/Header";
 
 export default async function LocaleLayout({
   children,
-  params: { locale },
+  // params: { locale },
 }) {
-  // Wrap params in a Promise if necessary
-  const resolvedParams = Promise.resolve({ locale });
 
-  if (!routing.locales.includes(locale)) {
-    notFound();
-  }
+  // const resolvedParams = Promise.resolve({ locale });
+  // if (!routing.locales.includes(locale)) {
+  //   notFound();
+  // }
 
   const messages = await getMessages();
 
   return (
-    <html lang={locale}>
+    <html >
       <body>
         <Header />
         <NextIntlClientProvider messages={messages}>
