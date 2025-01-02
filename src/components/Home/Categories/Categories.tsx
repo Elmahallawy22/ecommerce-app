1dea22708e0f4595bb1dee6148ffc5feaed5e01d
Elmahallawy22 <elmahallawyyoussef@gmail.com>
2025-01-02 03:02:29 +0200

    add section of categories

---
+++ src/components/Home/Categories/Categories.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import "./Categories.scss";
+import { useTranslations } from 'next-intl';
+import { Link } from '../../../i18n/routing';
+import Image, { StaticImageData } from 'next/image';
+import Men from "../assets/Men.png";
+import Women from "../assets/Women.png";
+import Children from "../assets/Children.png";
+import Beauty from "../assets/Beauty.png";
+import Electonics from "../assets/electronics.png";
+import Mobiles from "../assets/Mobiles.png";
+import Brands from "../assets/Brands.png";
+import Offers from "../assets/Offer.png";
+
+const Categories = () => {
+  const t = useTranslations("Categories");
+
+  const categoriesList: { id: number, item: string, image: StaticImageData }[] =
+    [
+      { id: 1, item: "women", image: Men },
+      { id: 2, item: "men", image: Women },
+      { id: 3, item: "children", image: Children },
+      { id: 4, item: "beauty", image: Beauty },
+      { id: 5, item: "electronics", image: Electonics },
+      { id: 6, item: "mobiles", image: Mobiles },
+      { id: 7, item: "brands", image: Brands },
+      { id: 8, item: "offers", image: Offers }
+    ];
+
+  return (
+    <div className='categories flex justify-center'>
+      <div className="container">
+        <ul className='w-full px-1'>
+          {categoriesList.map((category) => {
+            return (
+              <li key={category.id}>
+                <Image src={category.image} alt={`${category.image}`} loading="lazy" />
+                <Link href={`/categories/${category.item}`}>
+                  {t(category.item)}
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Categories;
